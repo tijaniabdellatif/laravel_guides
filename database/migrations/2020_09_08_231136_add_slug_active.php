9c07940292dea74e862f40ab326e78daaf2f058c
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2020-09-09 00:18:28 +0100

    'migration_artisan'

---
+++ database/migrations/2020_09_08_231136_add_slug_active.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugActive extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('slug',140);
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            // $table->dropColumn('slug');
+            // $table->dropColumn('active');
+
+            $table->dropColumn(['slug','active']);
+        });
+    }
+}
